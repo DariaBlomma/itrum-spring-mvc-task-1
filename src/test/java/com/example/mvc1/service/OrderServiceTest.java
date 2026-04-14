3ba512e6d1ad4eab8539ef395194698f4a0c0a77
Daria Kozina <kozina.flowers@yandex.ru>
2026-04-14 14:15:27 +0300

    finished order create tests

--- src/test/java/com/example/mvc1/service/OrderServiceTest.java
+++ src/test/java/com/example/mvc1/service/OrderServiceTest.java
@@ -2,10 +2,13 @@ package com.example.mvc1.service;
 
 import com.example.mvc1.dtos.order.OrderRequest;
 import com.example.mvc1.dtos.order.OrderResponse;
+import com.example.mvc1.entities.Order;
 import com.example.mvc1.entities.User;
 import com.example.mvc1.enums.OrderStatus;
 import com.example.mvc1.mappers.OrderMapperImpl;
+import com.example.mvc1.repositories.OrderRepository;
 import com.example.mvc1.services.OrderService;
+import org.aspectj.weaver.ast.Or;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -16,6 +19,8 @@ import org.springframework.boot.jpa.test.autoconfigure.TestEntityManager;
 import org.springframework.context.annotation.Import;
 import static org.assertj.core.api.Assertions.assertThat;
 import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.Optional;
 
 @DataJpaTest
 @Import({OrderService.class, OrderMapperImpl.class})
@@ -23,6 +28,9 @@ import java.math.BigDecimal;
 public class OrderServiceTest {
     @Autowired
     private OrderService orderService;
+
+    @Autowired
+    private OrderRepository orderRepository;
 
     @Autowired
     private TestEntityManager entityManager;
@@ -37,10 +45,38 @@ public class OrderServiceTest {
 
             Long userId = saveTestUser();
             OrderResponse response = orderService.create(userId, request);
+
             assertThat(response)
                     .usingRecursiveComparison()
                     .ignoringFields("id")
                     .isEqualTo(expectedResponse);
+
+            assertThat(response.getId()).isNotNull();
+            assertThat(response.getId()).isPositive();
+        }
+
+        @Test
+        void shouldBindOrderToUserWhenProvidedCorrectRequestAndExistingUser() {
+            OrderRequest request = new OrderRequest("order2", BigDecimal.valueOf(34.03), OrderStatus.PENDING);
+            Long userId = saveTestUser();
+
+            OrderResponse response = orderService.create(userId, request);
+            Order saved = orderRepository.findById(response.getId()).orElseThrow();
+
+            assertThat(saved.getUser().getId()).isEqualTo(userId);
+        }
+
+        @Test
+        void shouldNotSaveOrderToDBWhenProvidedUserIsInactive() {
+            OrderRequest request = new OrderRequest("order3", BigDecimal.valueOf(34.03), OrderStatus.PENDING);
+            Long inactiveUserId = saveInactiveTestUser();
+
+            try {
+                orderService.create(inactiveUserId, request);
+            } catch (RuntimeException ignored) {
+            }
+
+            assertThat(orderRepository.count()).isZero();
         }
 
         private Long saveTestUser() {
@@ -48,6 +84,17 @@ public class OrderServiceTest {
                     .email("mail1@gmail.com")
                     .userName("user1")
                     .color("purple")
+                    .build();
+            entityManager.persistAndFlush(user);
+            return user.getId();
+        }
+
+        private Long saveInactiveTestUser() {
+            User user = User.builder()
+                    .email("mail1@gmail.com")
+                    .userName("user1")
+                    .color("purple")
+                    .deletedAt(Instant.now())
                     .build();
             entityManager.persistAndFlush(user);
             return user.getId();
